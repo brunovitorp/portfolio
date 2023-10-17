7ea5b2b7f342cc70a2f5578ee7f7d44bda434b54
Bruno Vitor - Notebook <bvpn20@gmail.com>
2023-10-17 15:48:40 -0300

    adição de logo no header eajustes no css

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,17 @@
 import React from "react";
+import logo from "./port.png";
 
 function Header() {
   return (
     <header className="header">
+      <div className="logo-container">
+        <img src={logo} alt="Logo" className="logo" />
         <h1>Meu Portfólio</h1>
-      <nav className="navbar">
-        <a href="#projects">Projetos</a>
-        <a href="#contact">Contato</a>
-      </nav>
+      </div>
+        <nav className="navbar">
+          <a href="#projects">Projetos</a>
+          <a href="#contact">Contato</a>
+        </nav>
     </header>
   );
 }
