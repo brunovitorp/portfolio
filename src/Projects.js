5f1578d1e4252b8886e27f919da178ec52aed1b9
Bruno Vitor - Notebook <bvpn20@gmail.com>
2023-10-17 15:17:23 -0300

    Alterações em alguns valores do array

--- src/Projects.js
+++ src/Projects.js
@@ -3,10 +3,10 @@ import React from 'react';
 // Definindo um componente funcional chamado Projects
 function Projects() {
   // Criando um array de objetos chamado 'projects'
-  const projects = [
-    { title: 'Calculadora', description: 'Descrição do Projeto 1' },
-    { title: 'Alcool ou Gasolina', description: 'Descrição do Projeto 2' },
-    { title: 'IMC', description: 'Descrição do Projeto 3' },
+  const projetos = [
+    { titulo: 'Calculadora', descricao: 'Descrição do Projeto 1' },
+    { titulo: 'Alcool ou Gasolina', descricao: 'Descrição do Projeto 2' },
+    { titulo: 'IMC', descricao: 'Descrição do Projeto 3' },
   ];
 
   // O componente retorna JSX, representando a seção de projetos
@@ -18,12 +18,12 @@ function Projects() {
       {/* Lista de projetos */}
       <ul className="project-list">
         {/* Mapeando os projetos e criando um elemento <li> para cada um */}
-        {projects.map((project, index) => (
+        {projetos.map((project, index) => (
           <li key={index} className="project">
             {/* Título do projeto */}
-            <h3>{project.title}</h3>
+            <h3>{project.titulo}</h3>
             {/* Descrição do projeto */}
-            <p>{project.description}</p>
+            <p>{project.descricao}</p>
           </li>
         ))}
       </ul>
